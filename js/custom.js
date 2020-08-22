5609f3c3d25e054c7fd40107cbec0f8c02b3db17
Parker Chun <parkerchun@gmail.com>
2020-08-21 22:46:26 -0400

    more gifs

--- js/custom.js
+++ js/custom.js
@@ -5,20 +5,39 @@ var span = document.getElementsByClassName("close")[0];
 $("#own-it-btn").on("click", function() {
     $("#myModal").css("display", "block")
     $("#song-name").html("Own It")
-    $("#song-frame").attr("src", "https://www.youtube.com/embed/sSCxoe1iGGc")
+    $("#song-frame").attr("src", "https://www.youtube.com/embed/sSCxoe1iGGc?&autoplay=1")
 })
 
 $("#euphoria-btn").on("click", function() {
     $("#myModal").css("display", "block")
     $("#song-name").html("Euphoria")
-    $("#song-frame").attr("src", "https://www.youtube.com/embed/1IGQyoP7ZVs")
+    $("#song-frame").attr("src", "https://www.youtube.com/embed/1IGQyoP7ZVs?&autoplay=1")
 })
 
 $("#save-goodbye-btn").on("click", function() {
     $("#myModal").css("display", "block")
     $("#song-name").html("Save Your Goodbye")
-    $("#song-frame").attr("src", "https://www.youtube.com/embed/7aulG4pb9AA")
+    $("#song-frame").attr("src", "https://www.youtube.com/embed/7aulG4pb9AA?&autoplay=1")
 })
+
+$("#burn-btn").on("click", function() {
+    $("#myModal").css("display", "block")
+    $("#song-name").html("Burn")
+    $("#song-frame").attr("src", "https://www.youtube.com/embed/lnfqchbuS2A?&autoplay=1")
+})
+
+$("#hola-btn").on("click", function() {
+    $("#myModal").css("display", "block")
+    $("#song-name").html("Hola Hola")
+    $("#song-frame").attr("src", "https://www.youtube.com/embed/M8Eu_bVooBg?&autoplay=1")
+})
+
+$("#youth-btn").on("click", function() {
+    $("#myModal").css("display", "block")
+    $("#song-name").html("Youth")
+    $("#song-frame").attr("src", "https://www.youtube.com/embed/UVp-LQvSi7E?&autoplay=1")
+})
+
 
 // // When the user clicks on <span> (x), close the modal
 // span.onclick = function() {
